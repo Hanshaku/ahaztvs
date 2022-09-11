230b9f0ab23d931d13f0fee38bd0bb67d0b5e6fe
Akbar Febriansyah <akbarfeb@yahoo.com>
2022-09-11 09:36:05 +0700

    benerin template  login

--- resources/views/Layout/sidebarAdmin.blade.php
+++ resources/views/Layout/sidebarAdmin.blade.php
@@ -6,13 +6,7 @@
     <span class="brand-text font-weight-light invisible">SELAMAT DATANG</span>
   </a>
 
-  <!-- Sidebar -->
-  <div class="sidebar">
-    <!-- Sidebar user (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-          <img src="{{ asset('template/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-        </div>
+
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
